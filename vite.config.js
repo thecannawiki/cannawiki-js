671948033a687eb623ed8518365c17907f64b4e7
thecannawiki <thecannawiki@gmail.com>
2025-01-08 18:18:51 +0000

    image compression

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,20 @@ export default defineConfig({
       targets: [
         { src: "src/wiki/*", dest: "wiki" }, // Copy markdown files to the output
       ],
+    }),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+      svg: {
+          plugins: [
+              { name: 'removeViewBox', active: false },
+              { name: 'sortAttrs' },
+          ],
+      },
+      includePublic: true,
+      logStats:true
     }),
   ],
   assetsInclude: ['**/*.md']
