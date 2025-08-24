a31461d1a6490fb7bf1d5cd656e1e8513108e3b9
thecannawiki <thecannawiki@gmail.com>
2025-08-24 01:08:59 +0100

    Update Navmenu.tsx

--- src/Navmenu.tsx
+++ src/Navmenu.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {Link, useLocation } from "react-router-dom";
+import {Link} from "react-router-dom";
 // import "./Navmenu.css"
 import React from 'react';
 import { useOrientation } from "./providers/OrientationProvider";
